dfd1d7f9ab43e49717e450e528b3de17940acd17
ikkrit-johan <bouhbouh379@gmail.com>
2022-04-21 00:41:00 +0200

    gt

---
+++ src/pages/Portefolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Portefolio = () => {
+    return (
+        <div>
+            <Navigation />
+        </div>
+    );
+};
+
+export default Portefolio;
